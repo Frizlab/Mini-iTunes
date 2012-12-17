c257d34ece708dbba8e7f3b72e2f1c16eb7f0f2c
Frizlab <fload@me.com>
2012-12-18 00:26:49 +0100

    Current position and track length shown as text too on main window.

---
+++ Mini iTunes/FLSecondsToHMSTransformer.h
@@ -0,0 +1,13 @@
+/*
+ * FLSecondsToHMSTransformer.h
+ * Mini iTunes
+ *
+ * Created by François LAMBOLEY on 12/18/12.
+ * Copyright (c) 2012 Frost Land. All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface FLSecondsToHMSTransformer : NSValueTransformer
+
+@end
---
+++ Mini iTunes/FLSecondsToHMSTransformer.m
@@ -0,0 +1,35 @@
+/*
+ * FLSecondsToHMSTransformer.m
+ * Mini iTunes
+ *
+ * Created by François LAMBOLEY on 12/18/12.
+ * Copyright (c) 2012 Frost Land. All rights reserved.
+ */
+
+#import "FLSecondsToHMSTransformer.h"
+
+@implementation FLSecondsToHMSTransformer
+
++ (BOOL)allowsReverseTransformation
+{
+	return NO;
+}
+
+- (id)transformedValue:(id)value
+{
+	if (![value respondsToSelector:@selector(floatValue)])
+		[NSException raise:@"Invalid Value" format:@"Got value \"%@\" to transform into HMS. Cannot do that as value does not respond to floatValue", value];
+	
+	float seconds = [value floatValue];
+	BOOL minus = (seconds < 0);
+	if (minus) seconds = -seconds;
+	
+	unsigned h = (unsigned)(seconds / 3600.);
+	seconds -= h * 3600;
+	unsigned m = (unsigned)(seconds / 60.);
+	seconds -= m * 60;
+	unsigned s = (unsigned)seconds;
+	return [NSString stringWithFormat:@"%@%02u:%02u:%02u", minus? @"-": @"", h, m, s];
+}
+
+@end
